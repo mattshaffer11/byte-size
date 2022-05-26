05aff14d1bd55d2a1469773e0734e2879bd7521d
mattshaffer11 <mattshaffer11@gmail.com>
2022-05-26 12:59:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-etjves
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-etjves)
---
+++ index.ts
@@ -0,0 +1,34 @@
+import './style.css';
+
+import { merge, fromEvent, map } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+const decimal = document.querySelector('input[name="decimal"]');
+const hex = document.querySelector('input[name="hex"]');
+const binary = document.querySelector('input[name="binary"]');
+
+function createInputObservable(input, base) {
+  return fromEvent(input, 'keyup').pipe(
+    map((event) => {
+      const value = event.target.value.trim();
+      return value.length > 0 ? parseInt(value, base) : 0;
+    }),
+  );
+}
+
+let currentValue = 0;
+merge(
+  createInputObservable(decimal, 10),
+  createInputObservable(hex, 16),
+  createInputObservable(binary, 2),
+)
+  .pipe(
+    scan((current, value) => {
+      return Number.isNaN(value) ? current : value;
+    }, 0)
+  )
+  .subscribe((value: number) => {
+    decimal.value = value.toString(10);
+    hex.value = value.toString(16).toUpperCase();
+    binary.value = value.toString(2);
+  });
